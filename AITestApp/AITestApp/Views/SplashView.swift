94e4d90d3f6f6e34bcff95ba85a39c782599cee1
l_0045_r.watanabe <r.watanabe@sonicmoov.com>
2025-03-25 10:59:13 +0900

    Initial commit

---
+++ AITestApp/AITestApp/Views/SplashView.swift
@@ -0,0 +1,8 @@
+//
+//  SplashView.swift
+//  AITestApp
+//
+//  Created by L_0045_r.watanabe on 2025/03/25.
+//
+
+import Foundation
---
+++ AITestApp/AITestApp/Views/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  AITestApp
+//
+//  Created by L_0045_r.watanabe on 2025/03/25.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
